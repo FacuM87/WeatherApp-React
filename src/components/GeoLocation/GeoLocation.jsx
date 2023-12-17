3cfa4d0e003e483a9de16a01d68c6c286d718a4b
FacuM87 <facundo.mingorance@hotmail.com>
2023-12-17 20:51:41 -0300

    modificaciones varias

--- src/components/GeoLocation/GeoLocation.jsx
+++ src/components/GeoLocation/GeoLocation.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 
 const GeoLocationComponent = () => {
   const [location, setLocation] = useState(null);
+  const [geoData, setgeoData] = useState({
+	nombreCiudad:"",
+	pais:"",
+	region:"",
+	temperatura:"",
+	sensTerm:"",
+	texto:"",
+	icono:""
+  })
   
   useEffect(() => {
       if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });  
+          setLocation({ latitude, longitude });
         },
         (error) => {
             console.error('Error getting geolocation:', error.message);
@@ -17,21 +26,43 @@ const GeoLocationComponent = () => {
     } else {
         console.error('Geolocation is not supported by this browser.');
     }
-}, []);
+	}, []);
 
-console.log(location);
+	useEffect(() => {
+		if (location) {
+		const geolocation = `${location.latitude},${location.longitude}`;
+		console.log(geolocation);
+		fetch(`https://api.weatherapi.com/v1/current.json?key=5e92979a14bf4ee9a40181648231910&q=${geolocation}#`)
+		.then(response => {return response.json()})
+		  .then(data => {
+			console.log(data)
+			setgeoData({
+				nombreCiudad:data.location.name,
+				pais:data.location.country,
+				region:data.location.region,
+				temperatura:data.current.temp_c,
+				sensTerm:data.current.feelslike_c,
+				texto:data.current.condition.text,
+				icono:data.current.condition.icon
+			})
+		})
+		  .catch(error => {
+			console.error('Error:', error);
+		  });
+		}
+	}, [location]);
 
+	console.log(location);
+	console.log(geoData);
 
   return (
     <header>
-        <div>
-        {location ? (
-            <p>
-            Your current location is: {location.latitude}, {location.longitude}
-            </p>
-        ) : (
-            <p>Getting location...</p>
-        )}
+        <div className=''>
+			{location ? (
+				<p><img src={geoData.icono} alt="icono"/>{geoData.temperatura}°C, {geoData.nombreCiudad}, {geoData.region}, {geoData.pais}</p>
+			) : (
+				<p>Getting location...</p>
+			)}
         </div>
     </header>
   );

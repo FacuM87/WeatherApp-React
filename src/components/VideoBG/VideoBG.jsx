c0e76bd5da088f21928b944463b2088b45b3a847
FacuM87 <facundo.mingorance@hotmail.com>
2023-12-20 12:36:45 -0300

    incorpora bg-overlay

--- src/components/VideoBG/VideoBG.jsx
+++ src/components/VideoBG/VideoBG.jsx
@@ -5,6 +5,7 @@ import VideoBackGround from "../../assets/video-bg.mp4"
 const VideoBG = () => {
   return (
     <div>
+        <div className='bg-overlay'></div>
         <video className="videoBg" src={VideoBackGround} autoPlay muted loop />
     </div>
   )

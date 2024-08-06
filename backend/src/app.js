2907a89aceb1c5794654b9075704460a4b122096
FacuM87 <facundo.mingorance@hotmail.com>
2024-08-06 00:22:06 -0300

    nueva URL en origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 const allowedOrigin = config.origin
 console.log(allowedOrigin);
 //app.use(cors({credentials: true, origin: allowedOrigin}))
-app.use(cors({credentials: true, origin: "https://my-weather-app-ashy.vercel.app"}))
+app.use(cors({credentials: true, origin: "https://my-weather-app-bfvt-i1b8u8pmy-facum87s-projects.vercel.app/"}))
 
 /* Mongo DB connection */
 mongoose.connect(config.mongoUrl, {dbName: "weather-app"})

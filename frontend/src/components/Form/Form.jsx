ae146b4b326e581e130b20897e28a116082a974c
FacuM87 <facundo.mingorance@hotmail.com>
2025-08-02 22:13:47 -0300

    pequeña correccion en barra de busqueda

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import config from '../../config.js'
 import "./Form.css"
 import WeatherCard from '../WeatherCard/WeatherCard'
-// import { useDispatch } from 'react-redux'
-// import { login } from '../../redux/userSlice.js'
 
 
 const Form = () => {
-
-    // const dispatch = useDispatch()
      
     const apiUrl= config.weather_api_url 
     const [ciudad, setCiudad] = useState("")
@@ -45,41 +41,14 @@ const Form = () => {
             }        
     }
 
-    // const checkSession = async () => {
-    //     try {
-    //         const fetchUrl = process.env.REACT_APP_API_SESSION_URL
-    //          const response = await fetch (fetchUrl, {
-    //             method: "GET",
-    //             headers: {
-    //                 "Content-Type": "application/json"},
-    //         })
-    //         const data = await response.json()
-    //         console.log(data);
-    //         const userData = {
-    //             first_name: data.first_name,
-    //             last_name: data.last_name,
-    //             email: data.email,
-    //             role: data.role
-    //         };
-    //         console.log(userData);
-    //         dispatch(login(userData));
-
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
-    
-    // useEffect(() => {
-    //     checkSession()
-    // }, [])
-
     return (
         <main>
             <div className='cont d-flex flex-column justify-content-center align-items-center'>
                 <section className='container-fluid d-flex flex-column justify-content-center align-items-center'>
                     <h1>My Weather App</h1>
                     <form onSubmit={handleOnSubmit} className='d-flex flex-column w-50 justify-content-center align-items-center'>
-                        <input name='city' required type='text' className='mb-2 form-control' placeholder='Enter a city name' onChange={(e) => setCiudad(e.target.value)}></input>
+                        <input name='city' required type='text' className='mb-2 form-control text-center' placeholder='Enter a city or a country name' onChange={(e) => setCiudad(e.target.value)}></input>
+                        
                         {!loader? (<button type='submit' className='btn btn-primary w-50'> Search </button>):(
                             <button className="btn btn-primary" type="button" disabled>
                                 <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>

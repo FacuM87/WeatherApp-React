d74c9b63fba5e9e2b792043acb3d82a3e9b5ab05
FacuM87 <facundo.mingorance@hotmail.com>
2025-08-03 20:34:29 -0300

    implementación del auth  middleware

--- backend/src/router/users.router.js
+++ backend/src/router/users.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { createAdminUser, createUser, deleteUser, getUserById, getUsers } from '../controllers/users.controllers.js'
+import { auth } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
-router.get('/', getUsers)
+router.get('/', auth(['admin']), getUsers)
 router.get('/:id', getUserById)
 router.post('/register', createUser)
-router.post('/createAdmin', createAdminUser)
-router.post('/delete/:id', deleteUser)
+router.post('/createAdmin', auth(['admin']), createAdminUser)
+router.post('/delete/:id', auth(['admin']), deleteUser)
 
 export default router
